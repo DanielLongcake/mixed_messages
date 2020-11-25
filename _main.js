918043851a57e5615b2948e3dae815c90204b922
Daniel Longcake <daniel.longcake@gmail.com>
2020-11-25 11:14:53 +0800

    Added _main.js and declared variables

---
+++ _main.js
@@ -0,0 +1,18 @@
+//The arrays of words will be constructed in the form of [Message][Adjective][Noun]
+//i.e.  Your day will be very interesting
+//      [    Message   ] [adj] [  noun  ]
+
+//Starter Message Array...
+let messages = [];
+//Adjective Word Array...
+let adjectives = [];
+//Noun Word Array...
+let nouns = [];
+
+//Function that returns the randomly selected message...
+function getMessage() {
+    //Declare the message array 
+    let message = [];
+    //Declare the message string (for later use)
+    let messageStr;
+}
