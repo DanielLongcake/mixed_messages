ef8de337e5cfdf0a662a4ea8231cee5f95e70fc9
Daniel Longcake <daniel.longcake@gmail.com>
2020-11-25 11:40:56 +0800

    Finished Message Generator Function

--- _main.js
+++ _main.js
@@ -19,9 +19,9 @@ const messageFactory = (messages, adverbs, nouns) => {
     //Create the message object
     let message = {
         //Assign the varaibles to the result of a random number generator function...
-        _message:   messages[randomGenerator(messages.length - 1)],
-        _adverb:    adverbs[randomGenerator(adverbs.length - 1)],
-        _noun:      nouns[randomGenerator(nouns.length - 1)]
+        _message:   messages[randomGenerator(messages.length)],
+        _adverb:    adverbs[randomGenerator(adverbs.length)],
+        _noun:      nouns[randomGenerator(nouns.length)]
     }
     return message;
 }
@@ -30,13 +30,13 @@ const messageFactory = (messages, adverbs, nouns) => {
 function getMessage() {
     //Declare the message object 
     let message = messageFactory(messages, adverbs, nouns);
-    //Add the variables from the object to an array...
-    let messageArray = [];
-    messageArray.push(message._message, message._adverb, message._noun);
-    //Debugging 
-    console.log(messageArray);
-    //Declare the message string (for later use)
+    //Declare the message string
     let messageStr;
+    //Concat the message components to the string
+    messageStr = `${message._message} ${message._adverb} ${message._noun}`;
+    //Return the message...
+    return messageStr;
 }
 
-getMessage();
+//Print the message to the console
+console.log(getMessage());
